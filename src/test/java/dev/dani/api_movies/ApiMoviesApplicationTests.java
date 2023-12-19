63a74787367616ec699a9981cbf67a6a9cc117fd
Dani Gallo <danigallo@Dani.local>
2023-12-19 10:08:00 +0100

    first

---
+++ src/test/java/dev/dani/api_movies/ApiMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dani.api_movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
